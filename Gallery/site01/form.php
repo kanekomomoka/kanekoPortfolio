158459cc0228ac01c9c75085a1314de57f3df6e9
kanekomomoka <kaneko.momoka105@gmail.com>
2025-01-30 17:03:29 +0900

    test

--- Gallery/site01/form.php
+++ Gallery/site01/form.php
@@ -1,56 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>[公式]お問い合わせ/ 長野県飯田市にある給食を食べられる食堂 おいしい給食亭</title>
-    
-    <link rel="stylesheet" href="css/otoiawase.css" />
-    <link rel="icon" href="images/top/Logo01.png">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>[公式]お問い合わせ/ 長野県飯田市にある給食を食べられる食堂 おいしい給食亭</title>
+
+  <link rel="stylesheet" href="css/otoiawase.css" />
+  <link rel="icon" href="images/top/Logo01.png">
 </head>
+
 <body>
-        <!-- ヘッダー-->
-        <header class="c-header c-hamburger-menu"><!-- 追記 クラスを追記 -->
-      <a href="index.html" class="c-header__logo">
-           <img class="logo_main" src="images/top/Logo.png" alt="なつかし給食亭" >
-      </a>
-      <input type="checkbox" name="hamburger" id="hamburger" class="c-hamburger-menu__input"/><!-- 追記 idはlabelのforと同じにする -->
-      <label for="hamburger" class="c-hamburger-menu__bg"></label><!-- 追記 ハンバーガーメニュを開いた時の背景 -->
-      <ul class="c-header__list c-hamburger-menu__list"><!-- 追記 クラスを追記 -->
+  <!-- ヘッダー-->
+  <header class="c-header c-hamburger-menu"><!-- 追記 クラスを追記 -->
+    <a href="index.html" class="c-header__logo">
+      <img class="logo_main" src="images/top/Logo.png" alt="なつかし給食亭">
+    </a>
+    <input type="checkbox" name="hamburger" id="hamburger" class="c-hamburger-menu__input" /><!-- 追記 idはlabelのforと同じにする -->
+    <label for="hamburger" class="c-hamburger-menu__bg"></label><!-- 追記 ハンバーガーメニュを開いた時の背景 -->
+    <ul class="c-header__list c-hamburger-menu__list"><!-- 追記 クラスを追記 -->
 
-        <li class="c-header__list-item01">
-          <a href="index.html" class="c-header__list-link01">トップ</a>
-        </li>
-        <li class="c-header__list-item">
-          <a href="menu.html" class="c-header__list-link">こんだて</a>
-        </li>
-        <li class="c-header__list-item">
-          <a href="news.html" class="c-header__list-link">お知らせ</a>
-        </li>
-        <li class="c-header__list-item">
-          <a href="shop.html" class="c-header__list-link">お店の紹介</a>
-        </li>
-        <li class="c-header__list-item">
-          <a href="blog.html" class="c-header__list-link">コラム
-            
-          </a>
-        </li>
+      <li class="c-header__list-item01">
+        <a href="index.html" class="c-header__list-link01">トップ</a>
+      </li>
+      <li class="c-header__list-item">
+        <a href="menu.html" class="c-header__list-link">こんだて</a>
+      </li>
+      <li class="c-header__list-item">
+        <a href="news.html" class="c-header__list-link">お知らせ</a>
+      </li>
+      <li class="c-header__list-item">
+        <a href="shop.html" class="c-header__list-link">お店の紹介</a>
+      </li>
+      <li class="c-header__list-item">
+        <a href="blog.html" class="c-header__list-link">コラム
 
-        
-      </ul>
-      <label for="hamburger" class="c-hamburger-menu__button">
-        
-        
-        <!-- 追記 ハンバーガーメニューのボタン -->
-        <span class="c-hamburger-menu__button-mark"></span>
-        <span class="c-hamburger-menu__button-mark"></span>
-        <span class="c-hamburger-menu__button-mark"></span>
-      </label>
-    </header>
+        </a>
+      </li>
 
 
+    </ul>
+    <label for="hamburger" class="c-hamburger-menu__button">
 
-    <div class="main">
+
+      <!-- 追記 ハンバーガーメニューのボタン -->
+      <span class="c-hamburger-menu__button-mark"></span>
+      <span class="c-hamburger-menu__button-mark"></span>
+      <span class="c-hamburger-menu__button-mark"></span>
+    </label>
+  </header>
+
+
+
+  <div class="main">
     <div class="contact-form">
       <div class="form-title">お問い合わせ</div>
       <form method="post" action="sent.html">
@@ -61,25 +63,25 @@
         <select name="age">
           <option value="未選択">選択してください</option>
           <!-- for文を用いて6歳から100歳までをoptionで選べるようにしましょう -->
-          <?html 
-            for ($i = 6; $i <= 100; $i++) {
-              echo "<option value='{$i}'>{$i}</option>";
-            } 
+          <?php
+          for ($i = 6; $i <= 100; $i++) {
+            echo "<option value='{$i}'>{$i}</option>";
+          }
           ?>
         </select>
 
         <div class="form-item">お問い合わせの種類</div>
-        <?html 
-          $types = array('予約に関するお問い合わせ' ,'寄付に関するお問い合わせ', '取材・メディア関連のお問い合わせ', 'お持ち帰りに関するお問い合わせ', 'その他');
-         ?>
+        <?php
+        $types = array('予約に関するお問い合わせ', '寄付に関するお問い合わせ', '取材・メディア関連のお問い合わせ', 'お持ち帰りに関するお問い合わせ', 'その他');
+        ?>
         <!-- この下にselectタグを書いていきましょう -->
         <select name="category">
           <option value="未選択">選択してください</option>
-           <?html
-             foreach ($types as $type) {
-              echo "<option value='{$type}'>{$type}</option>";
-             }
-           ?>
+          <?php
+          foreach ($types as $type) {
+            echo "<option value='{$type}'>{$type}</option>";
+          }
+          ?>
 
         </select>
 
@@ -91,4 +93,5 @@
     </div>
   </div>
 </body>
+
 </html>
